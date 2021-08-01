97426eb1c82c491ceb63fb3588f5f641c2a7f4cc
Manish Rajani <manishrjn23@gmail.com>
2021-08-01 23:13:46 +0530

    shifted to atlas

--- src/env/development.ts
+++ src/env/development.ts
@@ -2,7 +2,7 @@
 
 export const envConfig: any = {
     database: {
-        MONGODB_URI: 'mongodb://localhost:27017/',
+        MONGODB_URI: 'mongodb+srv://manishrjn23:somethingtoremember@cluster0.o5avq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
         MONGODB_DB_MAIN: 'dev_DB'
     }
 };
